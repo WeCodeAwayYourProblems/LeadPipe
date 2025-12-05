0fa5884c962c80bbe309ac0f47c83c4e961ccd9e
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-05 14:16:46 -0700

    Update IDatSourceAsync.cs

--- LeadPipe.Infrastructure/Data/Source/IDataSourceAsync.cs
+++ LeadPipe.Infrastructure/Data/Source/IDataSourceAsync.cs
@@ -4,6 +4,6 @@ namespace LeadPipe.Infrastructure.Data.Source;
 
 public interface IDataSourceAsync<T>
 {
-    public Task<Result<List<T>>> LoadAsync();
+    Task<Result<List<T>>> LoadAsync();
     Task<Result<List<T>>> RefreshAsync();
 }

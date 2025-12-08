5e861f3d041a8bc5d2bf9c802445c4ec5db1f5ba
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-08 08:49:33 -0700

    Clean up tests

--- LeadPipe.Infrastructure.Test/DateTimeConversionTests/DateTimeConversionTest.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace LeadPipe.Infrastructure.Test.DateTimeConversionTests;
-
-public class DateTimeConversionTest
-{
-}
--- LeadPipe.Infrastructure.Test/DbCallTests/DbServiceTest.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace LeadPipe.Infrastructure.Test.DbCallTests;
-
-public class DbServiceTest
-{
-}

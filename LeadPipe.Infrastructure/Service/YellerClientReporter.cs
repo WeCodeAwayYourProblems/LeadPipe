129643f784ff41479bf1af726d6b07991f675ff8
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-10 13:34:01 -0700

    Reporter is a keyed item

--- LeadPipe.Infrastructure/Service/YellerClientReporter.cs
+++ LeadPipe.Infrastructure/Service/YellerClientReporter.cs
@@ -1,9 +1,11 @@
 ﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
 using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Interfaces.Core;
 
 namespace LeadPipe.Infrastructure.Service;
 
+[SourceKey(Source.Yeller)]
 public class YellerClientReporter : IReport<YellerReport>
 {
     public Task<Result> ReportData(List<YellerReport> d)

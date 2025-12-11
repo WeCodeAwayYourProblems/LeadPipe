b697dd2ed9c9fad5fae4c3f21e3fe97e614091ca
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-11 14:04:10 -0700

    Create new translation for YellerDto to Plumbing

---
+++ LeadPipe.Translation/Translate/DtoToVo/YellerDtoToPlumbing.cs
@@ -0,0 +1,55 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+using System.Data;
+
+namespace LeadPipe.Translation.Translate.DtoToVo;
+
+internal class YellerDtoToPlumbing : IDtoToVo<YellerDto, Plumbing>
+{
+    public Plumbing Translate(YellerDto data)
+    {
+        // Phone Number
+        PhoneNumber number = new(PhoneNumber.Default);
+        if (data.project?.survey_answers is SurveyAnswer[] answers)
+            foreach (SurveyAnswer ans in answers)
+                if (ans.answer_text is string[] answerText)
+                    foreach (string a in answerText)
+                        if (PhoneNumber.TryParse(a, out PhoneNumber? parsed) && parsed.Number != PhoneNumber.Default && parsed is not null)
+                        {
+                            number = parsed;
+                            break;
+                        }
+
+        // Date
+        DateTimeOffset date =
+            data.time_created is DateTime dtc
+                ? new(DateTime.SpecifyKind(dtc, DateTimeKind.Utc), TimeSpan.Zero)
+                : DateTimeOffset.MaxValue;
+
+        // Contents
+        IEnumerable<string> contentsStr = [];
+        if (data.project?.survey_answers is SurveyAnswer[] surveyAnswers)
+            contentsStr = surveyAnswers.Select(a =>
+            {
+                string question = a.question_text ?? "(Question text missing)";
+                string[] answers = a.answer_text ?? [];
+
+                return string.Join(" | ",
+                    answers.Length == 0
+                        ? [question, "(Answer missing)"]
+                        : [question, .. answers]
+                );
+            });
+        else contentsStr = [""];
+
+        string contents = string.Join(" <|> ", contentsStr);
+
+        return new(
+            PhoneNumber: number,
+            Date: date,
+            Contents: contents,
+            Source.Yeller
+        );
+    }
+}

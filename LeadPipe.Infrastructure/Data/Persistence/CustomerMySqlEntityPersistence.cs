2fd92ab18cd0a36babf1ab7c59aa7beaadb4d1d5
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-15 16:03:20 -0700

    Update CustomermySqlEntityPersistence.cs

--- LeadPipe.Infrastructure/Data/Persistence/CustomerMySqlEntityPersistence.cs
+++ LeadPipe.Infrastructure/Data/Persistence/CustomerMySqlEntityPersistence.cs
@@ -7,9 +7,10 @@ namespace LeadPipe.Infrastructure.Data.Persistence;
 
 internal class CustomerMySqlEntityPersistence(ICustomerMySqlRepository repo) : IDataPersistence<CustomerMySqlEntity>
 {
-    private readonly ICustomerMySqlRepository _repo = repo; 
+    private readonly ICustomerMySqlRepository _repo = repo;
     public async Task<Result> SaveAsync(List<CustomerMySqlEntity> t)
     {
-        Result<List<CustomerMySqlEntity>> added = await _repo.AddRangeAsync(t); return added;
+        Result<List<CustomerMySqlEntity>> added = await _repo.AddRangeAsync(t);
+        return added;
     }
 }

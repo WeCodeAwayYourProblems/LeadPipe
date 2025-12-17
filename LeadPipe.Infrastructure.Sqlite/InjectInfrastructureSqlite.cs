0d2b7518c2bf8559a6a8ef33c52262a6d891d98f
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-17 13:47:59 -0700

    Clean up InjectInfrastructureSqlite.cs

--- LeadPipe.Infrastructure.Sqlite/InjectInfrastructureSqlite.cs
+++ LeadPipe.Infrastructure.Sqlite/InjectInfrastructureSqlite.cs
@@ -30,36 +30,4 @@ public static class InjectInfrastructureSqlite
 
         return services;
     }
-    private static void RegisterRepositories(IServiceCollection services)
-    {
-        Assembly assembly = typeof(PlumbingRepository).Assembly;
-
-        // Find all non-abstract classes that inherit from PlumbingContextRepository<T>
-        IEnumerable<Type> implementations = assembly
-            .GetTypes()
-            .Where(t => t is { IsClass: true, IsAbstract: false } &&
-                        t.BaseType != null &&
-                        t.BaseType.IsGenericType &&
-                        t.BaseType.GetGenericTypeDefinition() == typeof(PlumbingContextRepository<>));
-
-        foreach (Type implementation in implementations)
-        {
-            // Find the first interface implemented by this class that inherits IRepository<T>
-            Type? iface = implementation
-                .GetInterfaces()
-                .FirstOrDefault(i =>
-                    i.IsGenericType &&
-                    i.GetGenericTypeDefinition() == typeof(IRepository<>)
-                    ||
-                    i.GetInterfaces().Any(ii =>
-                        ii.IsGenericType &&
-                        ii.GetGenericTypeDefinition() == typeof(IRepository<>))
-                );
-
-            if (iface is not null)
-            {
-                services.AddScoped(iface, implementation);
-            }
-        }
-    }
 }

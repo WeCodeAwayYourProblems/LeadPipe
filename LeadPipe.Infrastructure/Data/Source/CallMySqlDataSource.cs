fe36a6282fd0db87dfd7918f318a118964feb088
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-23 12:28:12 -0700

    Add CallMySqlDataSource.cs

---
+++ LeadPipe.Infrastructure/Data/Source/CallMySqlDataSource.cs
@@ -0,0 +1,24 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Entity.MySql;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Repository.MySql;
+
+namespace LeadPipe.Infrastructure.Data.Source;
+
+public sealed class CallMySqlDataSource(
+    ICallMySqlRepository repo
+    ) : IDataSourceAsync<CallMySqlEntity>
+{
+    private readonly ICallMySqlRepository _repo = repo;
+    public async Task<Result<List<CallMySqlEntity>>> LoadAsync()
+    {
+        DateTime dateFilter = DateTime.UtcNow.AddYears(-2);
+        Result<List<CallMySqlEntity>> found = await _repo.FindAsync(c => c.called_at_utc <= dateFilter, true);
+        return found;
+    }
+
+    public Task<Result<List<CallMySqlEntity>>> RefreshAsync()
+    {
+        throw new NotImplementedException();
+    }
+}

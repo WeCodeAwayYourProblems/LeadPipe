3a50d46f7be7613027fa2c4a5e0c1086494ab9d6
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-23 13:36:22 -0700

    Persisting calls should be consistent

--- LeadPipe.Infrastructure/Data/Persistence/CallEntityPersistence.cs
+++ LeadPipe.Infrastructure/Data/Persistence/CallEntityPersistence.cs
@@ -1,25 +1,7 @@
-﻿using CSharpFunctionalExtensions;
-using LeadPipe.Domain.ValueObjects;
-using LeadPipe.Infrastructure.Entity.Sqlite;
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
-using LeadPipe.Infrastructure.Interfaces.Translate;
 
 namespace LeadPipe.Infrastructure.Data.Persistence;
 
 internal sealed class CallEntityPersistence(ICallRepository repo) : Persistence<ICallRepository, CallEntity>(repo), IDataPersistence<CallEntity> { }
-
-internal sealed class CallPersistence(
-    IDataPersistence<CallEntity> persist,
-    IVoToEntity<Call, CallEntity> voToE
-    ) : IDataPersistence<Call>
-{
-    private readonly IDataPersistence<CallEntity> _persist = persist;
-    private readonly IVoToEntity<Call, CallEntity> _voToE = voToE;
-    public async Task<Result> SaveAsync(List<Call> t)
-    {
-        List<CallEntity> entities = [.. t.Select(_voToE.Translate)];
-        Result result = await _persist.SaveAsync(entities);
-        return result;
-    }
-}
---
+++ LeadPipe.Infrastructure/Data/Persistence/CallPersistence.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Data.Persistence;
+
+internal sealed class CallPersistence(
+    IDataPersistence<CallEntity> persist,
+    IVoToEntity<Call, CallEntity> voToE
+    ) : IDataPersistence<Call>
+{
+    private readonly IDataPersistence<CallEntity> _persist = persist;
+    private readonly IVoToEntity<Call, CallEntity> _voToE = voToE;
+    public async Task<Result> SaveAsync(List<Call> t)
+    {
+        List<CallEntity> entities = [.. t.Select(_voToE.Translate)];
+        Result result = await _persist.SaveAsync(entities);
+        return result;
+    }
+}

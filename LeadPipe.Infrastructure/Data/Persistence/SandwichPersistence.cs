fe56a3f5345c9163d3f8da7c7562df9cbd0e038a
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-23 14:40:51 -0700

    Subs and Sandwich persistences

---
+++ LeadPipe.Infrastructure/Data/Persistence/SandwichPersistence.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Data.Persistence;
+
+internal sealed class SandwichPersistence(
+    IDataPersistence<SubsEntity> persist,
+    IVoToEntity<Sandwich, SubsEntity> voToE
+    ) : IDataPersistence<Sandwich>
+{
+    private readonly IDataPersistence<SubsEntity> _persist = persist;
+    private readonly IVoToEntity<Sandwich, SubsEntity> _voToE = voToE;
+    public async Task<Result> SaveAsync(List<Sandwich> t)
+    {
+        List<SubsEntity> entities = [.. t.Select(_voToE.Translate)];
+        Result result = await _persist.SaveAsync(entities);
+        return result;
+    }
+}
---
+++ LeadPipe.Infrastructure/Data/Persistence/SubsEntityPersistence.cs
@@ -0,0 +1,7 @@
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+
+namespace LeadPipe.Infrastructure.Data.Persistence;
+
+internal sealed class SubsEntityPersistence(ISubsRepository repo) : Persistence<ISubsRepository, SubsEntity>(repo), IDataPersistence<SubsEntity> { }

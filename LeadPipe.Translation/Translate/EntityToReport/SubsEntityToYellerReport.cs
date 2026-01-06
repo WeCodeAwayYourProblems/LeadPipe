a1ad0ff463a0b589f69dd3646b7744a7d4947206
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 09:34:59 -0700

    Refactor from SubsPlumbingLinkToYellerReport to SubsEntityToYellerReport

---
+++ LeadPipe.Translation/Translate/EntityToReport/SubsEntityToYellerReport.cs
@@ -0,0 +1,26 @@
+﻿using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Translation.Translate.EntityToReport;
+
+internal sealed class SubsEntityToYellerReport : IEntityToReport<SubsEntity, ReportYeller>
+{
+    public ReportYeller Translate(SubsEntity sub)
+    {
+        long eventTime = sub.UnixDate;
+        UserData user = new()
+        {
+            ph = [sub.Number.ToString(), sub.Number2.ToString()],
+            country = [YellerReportHelper.Country]
+        };
+        CustomData custom = new()
+        {
+            currency = YellerReportHelper.Currency,
+            value = sub.Value
+        };
+        string eventid = sub.Id.ToString();
+        ReportYeller result = new() { event_id = eventid, event_time = eventTime, custom_data = custom, user_data = user };
+        return result;
+    }
+}

05cf3682640809f28d4609609926874bf27670eb
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 15:32:42 -0700

    Rename YellerReportService to Yeller CsvReportService

---
+++ LeadPipe.Infrastructure/Service/Report/YellerCsvReportService.cs
@@ -0,0 +1,15 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Attributes;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadPipe.Infrastructure.Service.Report;
+
+[ScheduleKey(Schedule.TwoDays)]
+internal sealed class YellerCsvReportService(
+    [FromKeyedServices(Source.Yeller)] ILoadData<Plumbing> load,
+    ITransform<Plumbing, ReportPlumbing> transform,
+    [FromKeyedServices(Source.Yeller)] IReport<ReportPlumbing> report
+    ) : ReportService<Plumbing, ReportPlumbing>(load, transform, report)
+{ }

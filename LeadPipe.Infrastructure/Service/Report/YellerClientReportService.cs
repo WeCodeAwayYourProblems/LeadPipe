07c9d96f7880c5e6d649e7c56b03749dc388373e
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 15:32:55 -0700

    Add YellerClientReportService

---
+++ LeadPipe.Infrastructure/Service/Report/YellerClientReportService.cs
@@ -0,0 +1,15 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Attributes;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadPipe.Infrastructure.Service.Report;
+
+[ScheduleKey(Schedule.Daily)]
+internal sealed class YellerClientReportService(
+    [FromKeyedServices(Source.Yeller)] ILoadData<Plumbing> load,
+    ITransform<Plumbing, ReportYeller> transform,
+    IReport<ReportYeller> report
+    ) : ReportService<Plumbing, ReportYeller>(load, transform, report)
+{ }

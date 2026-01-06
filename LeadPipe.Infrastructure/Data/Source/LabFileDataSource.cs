5564a245277494493ad777007b9c35466f4f0d3d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 16:16:17 -0700

    Types should be in separate files

---
+++ LeadPipe.Infrastructure/Data/Source/LabFileDataSource.cs
@@ -0,0 +1,11 @@
+﻿using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Service;
+using LeadPipe.Infrastructure.Settings;
+using Microsoft.Extensions.Logging;
+
+namespace LeadPipe.Infrastructure.Data.Source;
+
+internal sealed class LabFileDataSource(IInfrastructureSettings settings, ICsvRwService csv, IJsonRwService json, ILogger<LabFileDataSource> logging)
+    : FileDataSource<LabDto, LabFileDataSource>(new FileInfo(settings.LabSourceLoc!), csv, json, logging), IDataSourceAsync<LabDto>
+{ }
--- LeadPipe.Infrastructure/Data/Source/LeasedFileDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/LeasedFileDataSource.cs
@@ -9,7 +9,3 @@ namespace LeadPipe.Infrastructure.Data.Source;
 internal class LeasedFileDataSource(IInfrastructureSettings settings, ICsvRwService csv, IJsonRwService json, ILogger<LeasedFileDataSource> logging)
     : FileDataSource<LeasedDto, LeasedFileDataSource>(new FileInfo(settings.LeasedSourceLoc!), csv, json, logging), IDataSourceAsync<LeasedDto>
 { }
-
-internal sealed class LabFileDataSource(IInfrastructureSettings settings, ICsvRwService csv, IJsonRwService json, ILogger<LabFileDataSource> logging)
-    : FileDataSource<LabDto, LabFileDataSource>(new FileInfo(settings.LabSourceLoc!), csv, json, logging), IDataSourceAsync<LabDto>
-{ }

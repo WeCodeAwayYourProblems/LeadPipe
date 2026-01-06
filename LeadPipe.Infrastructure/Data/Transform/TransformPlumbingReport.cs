3df3c9a91ae191a4c02ab25d3090e1ec3ca66ccd
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 09:33:58 -0700

    Update TransformPlumbingReport.cs

--- LeadPipe.Infrastructure/Data/Transform/TransformPlumbingReport.cs
+++ LeadPipe.Infrastructure/Data/Transform/TransformPlumbingReport.cs
@@ -11,12 +11,12 @@ namespace LeadPipe.Infrastructure.Data.Transform;
 public sealed class TransformPlumbingReport(
     ISubsPlumbingLinkRepository repo,
     IVoToEntity<Plumbing, PlumbingEntity> voToEntity,
-    IEntityToReport<SubsPlumbingLink, ReportPlumbing> eToR
+    IEntityToReport<SubsEntity, ReportPlumbing> eToR
     ) : ITransform<Plumbing, ReportPlumbing>
 {
     private readonly ISubsPlumbingLinkRepository _repo = repo;
     private readonly IVoToEntity<Plumbing, PlumbingEntity> _voToEntity = voToEntity;
-    private readonly IEntityToReport<SubsPlumbingLink, ReportPlumbing> _eToR = eToR;
+    private readonly IEntityToReport<SubsEntity, ReportPlumbing> _eToR = eToR;
     public async Task<Result<List<ReportPlumbing>>> TransformAsync(List<Plumbing> data)
     {
         // Translate plumbing to plumbingentity
@@ -25,27 +25,30 @@ public sealed class TransformPlumbingReport(
         // Get links to the plumbing
         Result<List<SubsPlumbingLink>> linkResult = await _repo.GetAllWithDetailsAsync(plumbingEntities);
 
-        // Extract subs plumbing links
+        // Check Success
         List<SubsPlumbingLink>? links = linkResult.IsSuccess
             ? linkResult.Value
             : null;
         if (links is null)
             return Result.Failure<List<ReportPlumbing>>(linkResult.Error);
 
+        // Ensure the list is not null
+        List<SubsPlumbingLink> subsLinks = links!;
+
         // Turn subsplumbinglinks into a hashset of plumbingids for fast lookup
-        HashSet<long> ids = [.. links.Select(e => e.PlumbingId)];
+        HashSet<long> ids = [.. subsLinks.Select(e => e.PlumbingId)];
 
         // Turn empty PlumbingEntities into subsplumbinglinks
-        List<SubsPlumbingLink> unfoundPlumbing = 
+        List<SubsPlumbingLink> unfoundPlumbing =
             [.. plumbingEntities
                 .Where(e => !ids.Contains(e.Id)) // We are creating a partition
                 .Select(e => new SubsPlumbingLink { PlumbingEntity = e, SubsId = 0})
             ];
 
-        List<ReportPlumbing> result = 
+        List<ReportPlumbing> result =
             [
-                .. links.Select(_eToR.Translate),
-                .. unfoundPlumbing.Select(_eToR.Translate)
+                .. subsLinks.Select(s => _eToR.Translate(s.SubsEntity!)),
+                .. unfoundPlumbing.Select(p => _eToR.Translate(p.SubsEntity))
             ];
 
         return result;

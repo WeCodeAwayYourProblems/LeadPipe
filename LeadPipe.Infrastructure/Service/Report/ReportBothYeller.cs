adbfbbaf2f078ea68ca7886c5ffb55a9a7a4efb6
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 15:31:52 -0700

    Add a reporter that reports both types of yeller reports

---
+++ LeadPipe.Infrastructure/Service/Report/ReportBothYeller.cs
@@ -0,0 +1,52 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Application.Service;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadPipe.Infrastructure.Service.Report;
+
+[SourceKey(Source.Yeller)]
+internal sealed class ReportBothYeller(
+    [FromKeyedServices(Schedule.Daily)] IReportService<Plumbing> daily,
+    [FromKeyedServices(Schedule.TwoDays)] IReportService<Plumbing> twoDays
+    ) : IReportService<Plumbing>
+{
+    private readonly IReportService<Plumbing> _daily = daily;
+    private readonly IReportService<Plumbing> _twoDays = twoDays;
+    private List<Plumbing>? DailyData { get; set; }
+    private List<Plumbing>? TwoDaysData { get; set; }
+    public async Task<Result<List<Plumbing>>> GetDataAsync()
+    {
+        Result<List<Plumbing>> dailyData = await _daily.GetDataAsync();
+        if (dailyData.IsFailure)
+            return dailyData;
+        DailyData = dailyData.Value;
+        Result<List<Plumbing>> twoDaysData = await _twoDays.GetDataAsync();
+        if (twoDaysData.IsFailure)
+            return twoDaysData;
+        TwoDaysData = twoDaysData.Value;
+
+        List<Plumbing> result = [.. dailyData.Value, .. twoDaysData.Value];
+        return result;
+    }
+
+    public async Task<Result> ReportAsync(List<Plumbing> data)
+    {
+        if (DailyData is not List<Plumbing> daily || TwoDaysData is not List<Plumbing> twoDays)
+        {
+            string fault = (DailyData is null, TwoDaysData is null) switch
+            {
+                (true, false) => "Two Days Data",
+                (false, true) => "Daily Data",
+                _ => "Two Days Data and Daily Data"
+            };
+            return Result.Failure($"Did not fetch data properly. Culprit: {fault}");
+        }
+
+        Result dailyData = await _daily.ReportAsync(daily);
+        Result twoDaysData = await _twoDays.ReportAsync(twoDays);
+
+        return Result.Combine(dailyData, twoDaysData);
+    }
+}

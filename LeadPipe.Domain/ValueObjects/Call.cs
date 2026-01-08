8a9b21d2f4b67b2a94265127121c130989d09ab8
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-08 13:33:39 -0700

    Update Call.cs (remove unnecessary comment)

--- LeadPipe.Domain/ValueObjects/Call.cs
+++ LeadPipe.Domain/ValueObjects/Call.cs
@@ -1,4 +1,3 @@
 ﻿namespace LeadPipe.Domain.ValueObjects;
 
 public record Call(DateTimeOffset Date, PhoneNumber Number, TimeSpan Duration, string Note, string Source, bool Billable, string Location);
-// TODO: Add CornFormula

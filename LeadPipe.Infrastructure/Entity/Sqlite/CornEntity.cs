b26056658897addca2221980af9452842fb326d7
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-13 14:50:02 -0700

    Use string as source for  Corn

--- LeadPipe.Infrastructure/Entity/Sqlite/CornEntity.cs
+++ LeadPipe.Infrastructure/Entity/Sqlite/CornEntity.cs
@@ -1,8 +1,6 @@
-﻿using LeadPipe.Domain.ValueObjects;
+﻿namespace LeadPipe.Infrastructure.Entity.Sqlite;
 
-namespace LeadPipe.Infrastructure.Entity.Sqlite;
-
-public class CornEntity : ISourceEntity
+public class CornEntity : IEntity
 {
     public required long Id { get; set; }
     public required long PhoneNumber { get; set; }
@@ -10,7 +8,7 @@ public class CornEntity : ISourceEntity
     public required long UnixDate { get; set; }
     public required string Payload { get; set; }
     public required string MetaData { get; set; }
-    public required Source Source { get; set; }
+    public required string Source { get; set; }
 
     public ICollection<CustardCornLink> CustardCornLinks { get; set; } = [];
     public ICollection<SandCornLink> SandCornLinks { get; set; } = [];

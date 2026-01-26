e0ec2e228c1675fc465168dd788830b9d298bb40
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-26 16:48:42 -0700

    Update SandMySqlEntity.cs

--- LeadPipe.Infrastructure/Entity/MySql/SandMySqlEntity.cs
+++ LeadPipe.Infrastructure/Entity/MySql/SandMySqlEntity.cs
@@ -5,12 +5,12 @@ public class SandMySqlEntity
 #pragma warning disable IDE1006 // Naming Styles
     public int subscriptionID { get; set; }
     public int customerID { get; set; }
-    public DateTime dateAdded { get; set; }
+    public DateTime? dateAdded { get; set; }
     public int? active { get; set; }
     public decimal contractValue { get; set; }
     public string? serviceType { get; set; }
     public int? initialStatus { get; set; }
-    public DateTime dateCancelled { get; set; }
+    public DateTime? dateCancelled { get; set; }
     public int? soldBy { get; set; }
     public int? soldBy2 { get; set; }
     public int? soldBy3 { get; set; }

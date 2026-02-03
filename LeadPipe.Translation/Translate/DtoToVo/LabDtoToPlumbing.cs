46c86ca7e4f463020fcf5e7f19e3635f4fd00c90
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-03 13:29:50 -0700

    Update LabDtoToPlumbing.cs

--- LeadPipe.Translation/Translate/DtoToVo/LabDtoToPlumbing.cs
+++ LeadPipe.Translation/Translate/DtoToVo/LabDtoToPlumbing.cs
@@ -8,6 +8,28 @@ internal class LabDtoToPlumbing : IDtoToVo<LabDto, Plumbing>
 {
     public Plumbing Translate(LabDto dto)
     {
-        throw new NotImplementedException();
+        PhoneNumber number = dto.entities?.buyer is not null && PhoneNumber.TryParse(dto.entities?.buyer.telephone, out PhoneNumber p)
+            ? p
+            : new(PhoneNumber.Default);
+
+        DateTime d = DateTime.TryParse(dto.created_at?.date_utc, out DateTime r) ? r : DateTime.MinValue;
+        DateTimeOffset date = new(d, TimeSpan.Zero);
+
+        string contents = dto.display?.text is null ? string.Empty : dto.display.text;
+
+        string branch = "Unknown";
+        string location = dto.metadata?.location?.name is null ? "Unknown" : dto.metadata.location.name;
+        string metaData = $"Location: {location}";
+
+        Plumbing result = new(
+            Id: 0, 
+            PhoneNumber: number, 
+            Date: date, 
+            Contents: contents, 
+            Branch: branch, 
+            MetaData: metaData, 
+            Source: Source.Lab);
+        
+        return result;
     }
 }

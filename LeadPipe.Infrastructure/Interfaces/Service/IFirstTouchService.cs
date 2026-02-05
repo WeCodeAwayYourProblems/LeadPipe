399b852bc813ec9765e268cd0d1c1ef82e2dc017
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-05 15:43:57 -0700

    Add first touch service

---
+++ LeadPipe.Infrastructure/Interfaces/Service/IFirstTouchService.cs
@@ -0,0 +1,9 @@
+﻿using LeadPipe.Infrastructure.Interfaces.Core;
+
+namespace LeadPipe.Infrastructure.Interfaces.Service;
+
+internal interface IFirstTouchService
+{
+    TLink? GetFirstTouch<TLink>(IEnumerable<TLink> links)
+        where TLink : IEntity, IHasUnixMatchDate;
+}
---
+++ LeadPipe.Infrastructure/Service/FirstTouchService.cs
@@ -0,0 +1,14 @@
+﻿using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Service;
+
+namespace LeadPipe.Infrastructure.Service;
+
+internal class FirstTouchService : IFirstTouchService
+{
+    public TLink? GetFirstTouch<TLink>(IEnumerable<TLink> links) where TLink : IEntity, IHasUnixMatchDate
+    {
+        return links
+            .OrderBy(l => l.UnixMatchDate)
+            .FirstOrDefault();
+    }
+}

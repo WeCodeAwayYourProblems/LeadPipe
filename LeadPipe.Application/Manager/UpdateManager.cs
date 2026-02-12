54b4ad66244e0f90dd66eb3d84d8775ae6b22874
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 13:30:32 -0700

    Update UpdateManager.cs

--- LeadPipe.Application/Manager/UpdateManager.cs
+++ LeadPipe.Application/Manager/UpdateManager.cs
@@ -43,7 +43,7 @@ public sealed class UpdateManager(
         if (globals.IsFailure) return globals;
 
         // Update All Sources
-        Source[] sources = Enum.GetValues<Source>();
+        Source[] sources = [.. Enum.GetValues<Source>().Except([Source.Test, Source.Test2])];
         List<Result> results = new(sources.Length);
         foreach (var source in sources)
             results.Add(await RunSource(source, refresh));
@@ -61,7 +61,7 @@ public sealed class UpdateManager(
     #region Helpers
 
     private async Task<Result> RunSource(Source source, bool refresh)
-        => await RunIfDue(source, SyncKey.Plumbing, refresh, _update.GetService<Plumbing>(source));
+        => await RunIfDue(source, false, SyncKey.Plumbing, refresh, _update.GetService<Plumbing>(source));
 
     /// <summary>
     /// Whenever you need to update new global value objects, this is where they will update
@@ -70,26 +70,26 @@ public sealed class UpdateManager(
     /// <returns></returns>
     private async Task<Result> RunGlobals(bool refresh)
     {
-        Result caliperSaved = await RunIfDue(SyncKey.Caliper, refresh, _call);
+        Result caliperSaved = await RunIfDue(SyncKey.Caliper, refresh, false, _call);
         if (caliperSaved.IsFailure)
             return caliperSaved;
 
-        Result custardSaved = await RunIfDue(SyncKey.Custard, refresh, _custard);
+        Result custardSaved = await RunIfDue(SyncKey.Custard, refresh, true, _custard);
         if (custardSaved.IsFailure)
             return custardSaved;
 
-        Result sandwichSaved = await RunIfDue(SyncKey.Sandwich, refresh, _sandwich);
+        Result sandwichSaved = await RunIfDue(SyncKey.Sandwich, refresh, true, _sandwich);
         if (sandwichSaved.IsFailure)
             return sandwichSaved;
 
         return Result.Success();
     }
 
-    private static async Task<Result> UpdatedAndSaved<T>(bool refresh, IUpdateService<T> updateService)
+    private static async Task<Result> UpdatedAndSaved<T>(bool refresh, bool withDetails, IUpdateService<T> updateService)
     {
         Result<List<T>> updateData = refresh
-            ? await updateService.UpdateDataAsync()
-            : await updateService.GetDataAsync();
+            ? await updateService.UpdateDataAsync(withDetails)
+            : await updateService.GetDataAsync(withDetails);
         Result savedData = updateData.IsSuccess
             ? await updateService.SaveDataAsync(updateData.Value)
             : updateData;
@@ -111,13 +111,13 @@ public sealed class UpdateManager(
         return result;
     }
 
-    private async Task<Result> RunIfDue<T>(Source source, SyncKey key, bool refresh, IUpdateService<T> service)
+    private async Task<Result> RunIfDue<T>(Source source, bool withDetails, SyncKey key, bool refresh, IUpdateService<T> service)
     {
         bool shouldRun = await _syncGate.ShouldRunAsync(source, key);
         if (!shouldRun)
             return Result.Success();
 
-        Result result = await UpdatedAndSaved(refresh, service);
+        Result result = await UpdatedAndSaved(refresh, withDetails, service);
 
         if (result.IsSuccess)
             await _syncGate.MarkSuccessAsync(source, key);
@@ -127,13 +127,13 @@ public sealed class UpdateManager(
         return result;
     }
 
-    private async Task<Result> RunIfDue<T>(SyncKey key, bool refresh, IUpdateService<T> service)
+    private async Task<Result> RunIfDue<T>(SyncKey key, bool refresh, bool withDetails, IUpdateService<T> service)
     {
         bool shouldRun = await _syncGate.ShouldRunAsync(key);
         if (!shouldRun)
             return Result.Success();
 
-        Result result = await UpdatedAndSaved(refresh, service);
+        Result result = await UpdatedAndSaved(refresh, withDetails, service);
 
         if (result.IsSuccess)
             await _syncGate.MarkSuccessAsync(key);

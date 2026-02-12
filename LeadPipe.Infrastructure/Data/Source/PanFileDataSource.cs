9450ec6f72f24b35ccf129b58edb02619a12e7dc
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 16:12:54 -0700

    Update PanFieDataSource.cs

--- LeadPipe.Infrastructure/Data/Source/PanFileDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/PanFileDataSource.cs
@@ -1,4 +1,6 @@
-﻿using LeadPipe.Infrastructure.Dto;
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Service;
 using LeadPipe.Infrastructure.Settings;
@@ -8,4 +10,10 @@ namespace LeadPipe.Infrastructure.Data.Source;
 
 internal class PanFileDataSource(IInfrastructureSettings settings, ICsvRwService csv, IJsonRwService json, ILogger<PanFileDataSource> logging)
     : FileDataSource<PanDto, PanFileDataSource>(new FileInfo(settings.PanSourceLoc!), csv, json, logging), IDataSourceAsync<PanDto>
-{ }
+{
+    protected override Result<List<PanDto>> FlattenInvalid(Result<List<PanDto>> fileContents)
+    {
+        if (fileContents.IsFailure) return fileContents;
+        return fileContents.Value.Where(v => PhoneNumber.TryParse(v.Number, out var _)).ToList();
+    }
+}

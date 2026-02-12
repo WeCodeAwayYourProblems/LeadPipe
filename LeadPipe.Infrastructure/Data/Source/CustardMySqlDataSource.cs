be4f96728522d84938429f74386d1893fb422e78
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 13:32:31 -0700

    Update CustardMySqlDataSource.cs

--- LeadPipe.Infrastructure/Data/Source/CustardMySqlDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/CustardMySqlDataSource.cs
@@ -10,17 +10,17 @@ public sealed class CustardMySqlDataSource(
 ) : IDataSourceAsync<CustardMySqlEntity>
 {
     private readonly ICustardMySqlRepository _repo = repo;
-    public async Task<Result<List<CustardMySqlEntity>>> LoadAsync()
+    public async Task<Result<List<CustardMySqlEntity>>> LoadAsync(bool withDetails)
     {
         DateTime twentyTwelve = new(new DateOnly(2012, 1, 1), new TimeOnly(0), DateTimeKind.Utc);
-        Result<List<CustardMySqlEntity>> found = await _repo.FindAsync(s => s.dateAdded >= twentyTwelve, true);
+        Result<List<CustardMySqlEntity>> found = await _repo.FindAsync(s => s.dateAdded >= twentyTwelve, withDetails);
         return found;
     }
 
-    public async Task<Result<List<CustardMySqlEntity>>> RefreshAsync()
+    public async Task<Result<List<CustardMySqlEntity>>> RefreshAsync(bool withDetails)
     {
         DateTime lastMonth = DateTime.Now.AddDays(-30);
-        Result<List<CustardMySqlEntity>> found = await _repo.FindAsync(s => s.dateAdded >= lastMonth, true);
+        Result<List<CustardMySqlEntity>> found = await _repo.FindAsync(s => s.dateAdded >= lastMonth, withDetails);
         return found;
     }
 }

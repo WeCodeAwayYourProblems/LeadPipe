b2e64ff85158f656ec0fc3276700e900b1b39124
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 16:12:14 -0700

    Update LatherFileDataSource.cs

---
+++ LeadPipe.Infrastructure/Data/Source/LatherFileDataSource.cs
@@ -0,0 +1,23 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Service;
+using LeadPipe.Infrastructure.Settings;
+using Microsoft.Extensions.Logging;
+
+namespace LeadPipe.Infrastructure.Data.Source;
+
+public sealed class LatherFileDataSource(
+    IInfrastructureSettings settings,
+    ICsvRwService csv,
+    IJsonRwService json,
+    ILogger<LatherFileDataSource> logging)
+    : FileDataSource<LatherDto, LatherFileDataSource>(new FileInfo(settings.LatherSourceLoc!), csv, json, logging), IDataSourceAsync<LatherDto>
+{
+    protected override Result<List<LatherDto>> FlattenInvalid(Result<List<LatherDto>> fileContents)
+    {
+        if (fileContents.IsFailure) return fileContents;
+        return fileContents.Value.Where(v => PhoneNumber.TryParse(v.Phone, out PhoneNumber _)).ToList();
+    }
+}

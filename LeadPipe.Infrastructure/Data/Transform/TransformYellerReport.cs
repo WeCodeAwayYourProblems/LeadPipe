a82ec0bd41c2e1b1f587f23183570628b88e8d90
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 09:00:32 -0700

    Update TransformYellerReport.cs

--- LeadPipe.Infrastructure/Data/Transform/TransformYellerReport.cs
+++ LeadPipe.Infrastructure/Data/Transform/TransformYellerReport.cs
@@ -4,7 +4,6 @@ using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Entity.Sqlite;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
-using LeadPipe.Infrastructure.Interfaces.Service;
 using LeadPipe.Infrastructure.Interfaces.Translate;
 using LeadPipe.Infrastructure.Settings;
 
@@ -12,9 +11,6 @@ namespace LeadPipe.Infrastructure.Data.Transform;
 
 internal sealed class TransformYellerReport(
     IRepositoryFactory factory,
-    IEntityToReport<PlumbingEntity, ReportYeller> plumbToR,
-    IEntityToReport<CaliperEntity, ReportYeller> caliperToR,
-    IEntityToReport<CornEntity, ReportYeller> cornToR,
     IEntityToReport<CustardPlumbingLink, ReportYeller> cpLinkToR,
     IEntityToReport<CustardCaliperLink, ReportYeller> custCalToR,
     IEntityToReport<CustardCornLink, ReportYeller> custCornToR,
@@ -26,10 +22,6 @@ internal sealed class TransformYellerReport(
     private readonly IRepository<CaliperEntity> _caliperRepo = factory.GetRepository<CaliperEntity>();
     private readonly IRepository<CornEntity> _cornRepo = factory.GetRepository<CornEntity>();
     private readonly IRepository<PlumbingEntity> _plumbRepo = factory.GetRepository<PlumbingEntity>();
-
-    private readonly IEntityToReport<PlumbingEntity, ReportYeller> _plumbToR = plumbToR;
-    private readonly IEntityToReport<CaliperEntity, ReportYeller> _caliperToR = caliperToR;
-    private readonly IEntityToReport<CornEntity, ReportYeller> _cornToR = cornToR;
 
     private readonly IEntityToReport<CustardPlumbingLink, ReportYeller> _cpLinkToR = cpLinkToR;
     private readonly IEntityToReport<CustardCaliperLink, ReportYeller> _custCalToR = custCalToR;
@@ -45,43 +37,33 @@ internal sealed class TransformYellerReport(
 
         // Load all entities relevant to Yeller
         // These entities do not need navigation properties loaded
-        Result<List<CaliperEntity>> calipers = await _caliperRepo.FindAsync(c => c.Source == _settings.YellerCaliperSource1 || c.Source == _settings.YellerCaliperSource2); // This source is a specific string
+        Result<List<CaliperEntity>> calipers = 
+            await _caliperRepo.FindAsync(c => 
+                c.Source == _settings.YellerCaliperSource1 || 
+                c.Source == _settings.YellerCaliperSource2); // This source is a specific string
         if (calipers.IsFailure) return Result.Failure<List<ReportYeller>>(calipers.Error);
 
-        Result<List<CornEntity>> corns = await _cornRepo.FindAsync(c => c.Source == _settings.YellerCornSource); // This source is a specific string
+        Result<List<CornEntity>> corns = 
+            await _cornRepo.FindAsync(c => 
+                c.Source == _settings.YellerCornSource); // This source is a specific string
         if (corns.IsFailure) return Result.Failure<List<ReportYeller>>(corns.Error);
 
-        Result<List<PlumbingEntity>> plumbs = await _plumbRepo.FindAsync(c => c.Source == Domain.ValueObjects.Source.Yeller); // This source is an enum with db conversion to string
+        Result<List<PlumbingEntity>> plumbs = 
+            await _plumbRepo.FindAsync(c => 
+                c.Source == Domain.ValueObjects.Source.Yeller); // This source is an enum with db conversion to string
         if (plumbs.IsFailure) return Result.Failure<List<ReportYeller>>(plumbs.Error);
 
         // Load CustardEntities --> getting them with details is less expensive than getting the details separately
         HashSet<long> plumbLookup = [.. data.Select(x => x.Id)];
         HashSet<long> caliperLookup = [.. calipers.Value.Select(x => x.Id)];
         HashSet<long> cornLookup = [.. corns.Value.Select(x => x.Id)];
+
         Result<List<CustardEntity>> custards = await _custardRepo.FindWithDetailsAsync(c =>
                 c.CustardPlumbingLinks.Any(link => plumbLookup.Contains(link.PlumbingId)) ||
                 c.CustardCaliperLinks.Any(link => caliperLookup.Contains(link.CaliperId)) ||
                 c.CustardCornLinks.Any(link => cornLookup.Contains(link.CornId))
             ); // Retrieves RELEVANT custard entities instead of ALL custard entities
         if (custards.IsFailure) return Result.Failure<List<ReportYeller>>(custards.Error);
-
-        //*********************************************************************************
-        // Filter out entities that are associated with a custard,
-        // in order to create a partition -> Custard/NonCustard entities
-        //*********************************************************************************
-
-        List<CustardCaliperLink> custardCalipers = [.. custards.Value.SelectMany(c => c.CustardCaliperLinks)];
-        HashSet<long> custardCaliperIds = [.. custardCalipers.Select(c => c.CaliperId)];
-        List<CaliperEntity> nonCustardCalipers = [.. calipers.Value.Where(c => !custardCaliperIds.Contains(c.Id))];
-
-        List<CustardCornLink> custardCorns = [.. custards.Value.SelectMany(c => c.CustardCornLinks)];
-        HashSet<long> custardCornIds = [.. custardCorns.Select(c => c.CornId)];
-        List<CornEntity> nonCustardCorns = [.. corns.Value.Where(c => !custardCornIds.Contains(c.Id))];
-
-        List<CustardPlumbingLink> custardPlumbs = [.. custards.Value.SelectMany(c => c.CustardPlumbingLinks)];
-        HashSet<long> plumbingIds = [.. custardPlumbs.Select(d => d.PlumbingId)];
-        List<PlumbingEntity> nonCustardPlumbs = [.. plumbs.Value.Where(c => !plumbingIds.Contains(c.Id))];
-
 
         //*********************************************************************************
         // Associations
@@ -94,9 +76,12 @@ internal sealed class TransformYellerReport(
         // 2. Be earlier than all other entities
 
         // Build dictionaries for quick lookup
-        Dictionary<long, CaliperEntity> caliperById = calipers.Value.ToDictionary(c => c.Id);
-        Dictionary<long, CornEntity> cornById = corns.Value.ToDictionary(c => c.Id);
-        Dictionary<long, PlumbingEntity> plumbById = plumbs.Value.ToDictionary(c => c.Id);
+        Dictionary<long, CaliperEntity> caliperById = 
+            calipers.Value.ToDictionary(c => c.Id);
+        Dictionary<long, CornEntity> cornById = 
+            corns.Value.ToDictionary(c => c.Id);
+        Dictionary<long, PlumbingEntity> plumbById = 
+            plumbs.Value.ToDictionary(c => c.Id);
 
         // Flatten the relationship between a custard, <link>, and <entity>
         IEnumerable<CustardAssociation<CaliperEntity, CustardCaliperLink>> custardCaliperAssociations =
@@ -122,59 +107,22 @@ internal sealed class TransformYellerReport(
         IEnumerable<CustardAssociation<CornEntity, CustardCornLink>> cornAttributable = Attributable(custardCornAssociations);
         IEnumerable<CustardAssociation<PlumbingEntity, CustardPlumbingLink>> plumbAttributable = Attributable(custardPlumbAssociations);
 
-        // Non-attributable associations
-        HashSet<long> caliperAttributableIds = [.. caliperAttributable.Select(a => a.Entity.Id)];
-        HashSet<long> cornAttributableIds = [.. cornAttributable.Select(a => a.Entity.Id)];
-        HashSet<long> plumbAttributableIds = [.. plumbAttributable.Select(a => a.Entity.Id)];
-
         //*********************************************************************************
         // Report
         //*********************************************************************************
-        
-        // Non-custard entities can now be translated into reports
-        List<ReportYeller> nonCustardCaliperReports = [.. nonCustardCalipers.Select(_caliperToR.Translate)];
-        List<ReportYeller> nonCustardCornReports = [.. nonCustardCorns.Select(_cornToR.Translate)];
-        List<ReportYeller> nonCustardPlumbReports = [.. nonCustardPlumbs.Select(_plumbToR.Translate)];
-
-        // Report non-attributable associations
-        IEnumerable<ReportYeller> caliperNonAttributableReport =
-            custardCaliperAssociations
-                .Where(a => !caliperAttributableIds.Contains(a.Entity.Id))
-                .Select(c => _caliperToR.Translate(c.Entity));
-        IEnumerable<ReportYeller> cornNonAttributableReport =
-            custardCornAssociations
-                .Where(a => !cornAttributableIds.Contains(a.Entity.Id))
-                .Select(c => _cornToR.Translate(c.Entity));
-        IEnumerable<ReportYeller> plumbNonAttributableReport =
-            custardPlumbAssociations
-                .Where(a => !plumbAttributableIds.Contains(a.Entity.Id))
-                .Select(c => _plumbToR.Translate(c.Entity));
 
         // Report attributable associations
-        IEnumerable<ReportYeller> caliperAttributableReport = caliperAttributable
-            .Select(c => _custCalToR.Translate(c.Link));
-        IEnumerable<ReportYeller> cornAttributableReport = cornAttributable
-            .Select(c => _custCornToR.Translate(c.Link));
-        IEnumerable<ReportYeller> plumbAttributableReport = plumbAttributable
-            .Select(c => _cpLinkToR.Translate(c.Link));
-
-        // Gather reports
         List<ReportYeller> reports =
         [
-            .. nonCustardCaliperReports,
-            .. nonCustardCornReports,
-            .. nonCustardPlumbReports,
-            .. caliperNonAttributableReport,
-            .. cornNonAttributableReport,
-            .. plumbNonAttributableReport,
-            .. caliperAttributableReport,
-            .. cornAttributableReport,
-            .. plumbAttributableReport
+            .. caliperAttributable.Select(c => _custCalToR.Translate(c.Link)),
+            .. cornAttributable.Select(c => _custCornToR.Translate(c.Link)),
+            .. plumbAttributable.Select(c => _cpLinkToR.Translate(c.Link))
         ];
 
         return reports;
 
     }
+
     static IEnumerable<CustardAssociation<TEntity, TLink>> Attributable<TEntity, TLink>(IEnumerable<CustardAssociation<TEntity, TLink>> associations)
     {
         IEnumerable<CustardAssociation<TEntity, TLink>> result = associations
@@ -198,5 +146,7 @@ internal sealed class TransformYellerReport(
         bool result = a.EntityDate < cutoffDates.Min();
         return result;
     }
+
     record CustardAssociation<T, TLink>(TLink Link, T Entity, CustardEntity Custard, DateTime EntityDate);
+
 }

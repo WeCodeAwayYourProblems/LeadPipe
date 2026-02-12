040d76fab57cd3534792d1afb84d74f7dd45c489
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 13:29:42 -0700

    Update ReportManager.cs

--- LeadPipe.Application/Manager/ReportManager.cs
+++ LeadPipe.Application/Manager/ReportManager.cs
@@ -17,7 +17,7 @@ public sealed class ReportManager(
     public async Task<Result> Manage(Source source)
     {
         IReportService<Plumbing> reporter = _report.GetService(source);
-        Result<List<Plumbing>> reportData = await reporter.GetDataAsync();
+        Result<List<Plumbing>> reportData = await reporter.GetDataAsync(false);
         Result reported = reportData.IsSuccess
             ? await reporter.ReportAsync(reportData.Value)
             : reportData;
@@ -26,13 +26,10 @@ public sealed class ReportManager(
 
     public async Task<Result> Manage()
     {
-        Source[] sources = Enum.GetValues<Source>();
+        Source[] sources = [.. Enum.GetValues<Source>().Except([Source.Test, Source.Test2])];
         List<Result> results = new(sources.Length);
         foreach (Source source in sources)
         {
-            if (source == Source.Test || source == Source.Test2)
-                continue;
-
             Result reported = await Manage(source);
             results.Add(reported);
         }

0be24214800fdfb8fd2d3b82ea4c0315b5c971ac
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-13 11:53:54 -0700

    Add CornEntityPersistence.cs

---
+++ LeadPipe.Infrastructure/Data/Persistence/CornEntityPersistence.cs
@@ -0,0 +1,7 @@
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+
+namespace LeadPipe.Infrastructure.Data.Persistence;
+
+internal sealed class CornEntityPersistence(IRepository<CornEntity> repo): Persistence<IRepository<CornEntity>, CornEntity>(repo), IDataPersistence<CornEntity> { }

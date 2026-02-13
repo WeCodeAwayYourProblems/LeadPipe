fb1291cccdded02931f02c4ccccbac3b55ce15fc
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-13 11:54:43 -0700

    Add CornUpdateService.cs

---
+++ LeadPipe.Infrastructure/Service/Update/CornUpdateService.cs
@@ -0,0 +1,14 @@
+﻿using LeadPipe.Application.Service;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.MySql;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Service.Update;
+
+internal sealed class CornUpdateService(
+    IDataSourceAsync<CornMySqlEntity> corn,
+    IEntityToVo<CornMySqlEntity, CornFormula> eToVo,
+    IDataPersistence<CornFormula> persist
+    ) : ValueObjectUpdateService<CornMySqlEntity, CornFormula>(corn, eToVo, persist), IUpdateService<CornFormula>
+{ }

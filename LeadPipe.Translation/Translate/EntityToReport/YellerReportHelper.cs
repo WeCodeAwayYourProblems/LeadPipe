0d7141fd0af195f76c5b01639f8f144cfa4d5934
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-20 09:48:39 -0700

    Update YellerReportHelper.cs Make public

--- LeadPipe.Translation/Translate/EntityToReport/YellerReportHelper.cs
+++ LeadPipe.Translation/Translate/EntityToReport/YellerReportHelper.cs
@@ -3,11 +3,11 @@ using System.Text;
 
 namespace LeadPipe.Translation.Translate.EntityToReport;
 
-internal sealed class YellerReportHelper
+public sealed class YellerReportHelper
 {
-    internal const string Currency = "USD";
-    internal const string Country = "us";
-    internal static string HashSha256(string input)
+    public const string Currency = "USD";
+    public const string Country = "us";
+    public static string HashSha256(string input)
     {
         byte[] bytes = Encoding.UTF8.GetBytes(input);
         byte[] hash = SHA256.HashData(bytes);

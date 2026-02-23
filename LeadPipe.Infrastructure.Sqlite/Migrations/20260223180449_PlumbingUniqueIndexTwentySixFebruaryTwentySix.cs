62b1435308a8065ad05fe049a4f8a9895037f362
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-23 11:07:17 -0700

    Migrations added

---
+++ LeadPipe.Infrastructure.Sqlite/Migrations/20260223180449_PlumbingUniqueIndexTwentySixFebruaryTwentySix.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeadPipe.Infrastructure.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlumbingUniqueIndexTwentySixFebruaryTwentySix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlumbingEntities_PhoneNumber_Date_Source",
+                table: "PlumbingEntities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlumbingEntities_PhoneNumber_UnixDate_Source_MetaData",
+                table: "PlumbingEntities",
+                columns: new[] { "PhoneNumber", "UnixDate", "Source", "MetaData" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PlumbingEntities_PhoneNumber_UnixDate_Source_MetaData",
+                table: "PlumbingEntities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlumbingEntities_PhoneNumber_Date_Source",
+                table: "PlumbingEntities",
+                columns: new[] { "PhoneNumber", "Date", "Source" });
+        }
+    }
+}

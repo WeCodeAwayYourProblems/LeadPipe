a5d603cd37b7096c0561f8ad4fec299899e8cb9b
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-02 11:35:53 -0700

    Add CatManUpdate.cs

---
+++ LeadPipe.Cli/Verbs/CatManUpdate.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using CSharpFunctionalExtensions;
+using LeadPipe.Application.Manager;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadPipe.Cli.Verbs;
+
+[Verb(Name, HelpText = "Update CatMan data.")]
+internal class CatManUpdate : IVerbAsync
+{
+    private const string Name = "catman";
+
+    #region Options
+    [Option('r', "refresh", Default = false, HelpText = "Whether or not to performa refresh. If not, all data will be retrieved. Defaults to false")]
+    public bool Refresh { get; set; }
+    #endregion
+
+    #region Public (Other than Options)
+    public async Task<int> Run(IServiceProvider service)
+    {
+        ICatManManager manager = service.GetRequiredService<ICatManManager>();
+        Result result = await manager.Manage(Refresh);
+        
+        int code = result.IsSuccess ? 0 : 1;
+        Environment.ExitCode = code;
+        
+        return code;
+    }
+    #endregion
+
+}

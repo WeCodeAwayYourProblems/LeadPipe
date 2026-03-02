9560913baa68a7c24962ee2727f71a7d8ed916a0
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-02 10:21:30 -0700

    Update CatManUpdateService.cs

---
+++ LeadPipe.Infrastructure/Service/Update/CatManUpdateService.cs
@@ -0,0 +1,16 @@
+﻿using LeadPipe.Application.Service;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Service.Update;
+
+internal sealed class CatManUpdateService(
+    IDataSourceAsync<CatManDto> source,
+    IDtoToVo<CatManDto, Caliper> dtoToVo,
+    IVoToEntity<Caliper, CaliperEntity> voToEntity,
+    IDataPersistence<CaliperEntity> persistence
+    ) : UpdateService<CatManDto, Caliper, CaliperEntity>(source, dtoToVo, voToEntity, persistence), IUpdateService<Caliper>
+{ }

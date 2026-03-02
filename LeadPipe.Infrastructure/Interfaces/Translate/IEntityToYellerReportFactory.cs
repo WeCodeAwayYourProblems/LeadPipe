2e8507f6be1257d9e89845f50baf0cf3fba2d2ce
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-02 13:18:23 -0700

    Create IEntityToYellerReportFactory.cs

---
+++ LeadPipe.Infrastructure/Interfaces/Translate/IEntityToYellerReportFactory.cs
@@ -0,0 +1,8 @@
+﻿using LeadPipe.Infrastructure.Dto;
+
+namespace LeadPipe.Infrastructure.Interfaces.Translate;
+
+public interface IEntityToYellerReportFactory
+{
+    IEntityToReport<TEntity, ReportYeller> GetService<TEntity>();
+}

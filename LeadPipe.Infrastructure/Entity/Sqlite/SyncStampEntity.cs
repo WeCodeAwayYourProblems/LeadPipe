673dc0b06124d456ddbcb939cb8207caeb7537b0
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-12 13:28:55 -0600

    Update SyncStampEntity.cs

---
+++ LeadPipe.Infrastructure/Entity/Sqlite/SyncStampEntity.cs
@@ -0,0 +1,13 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Interfaces.Core;
+
+namespace LeadPipe.Infrastructure.Entity.Sqlite;
+
+public class SyncStampEntity : IEntity
+{
+    public long Id { get; set; }
+    public required SyncKey Key { get; set; }
+    public Source? Source { get; set; }
+    public long UnixSyncUtc { get; set; }
+    public bool SuccessState { get; set; }
+}

3d9805ed563a9da9851f23279e1bfbe2e64c9122
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-24 09:16:42 -0600

    Update SandEntityToReportYeller.cs

--- LeadPipe.Translation/Translate/EntityToReport/SandEntityToReportYeller.cs
+++ LeadPipe.Translation/Translate/EntityToReport/SandEntityToReportYeller.cs
@@ -6,10 +6,10 @@ using LeadPipe.Infrastructure.Settings;
 
 namespace LeadPipe.Translation.Translate.EntityToReport;
 
-internal sealed class SandEntityToReportYeller(IYellerSettings settings) : IEntityToReport<SandEntity, ReportYeller>
+internal sealed class SandEntityToReportYellerOld(IYellerSettings settings) : IEntityToReport<SandEntity, ReportYeller_Old>
 {
     private readonly string _action = settings.YellerActionSource!;
-    public ReportYeller Translate(SandEntity sub)
+    public ReportYeller_Old Translate(SandEntity sub)
     {
         if(sub.CustardEntity is null)
             throw new ArgumentNullException(nameof(sub),$"{nameof(SandEntity)} cannot have a null {nameof(CustardEntity)}");
@@ -33,7 +33,7 @@ internal sealed class SandEntityToReportYeller(IYellerSettings settings) : IEnti
         };
         string eventid = sub.Id.ToString() + "-19114"; // 19 == s, 1 == a, 14 == n
 
-        ReportYeller result = new()
+        ReportYeller_Old result = new()
         {
             event_id = eventid,
             event_name = eventName,

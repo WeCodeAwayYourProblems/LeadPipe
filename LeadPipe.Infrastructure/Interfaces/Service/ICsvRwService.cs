12d93014a2705c71e7533c64c10b677a714815c9
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-24 11:51:57 -0600

    Update ICsvRwService.cs

--- LeadPipe.Infrastructure/Interfaces/Service/ICsvRwService.cs
+++ LeadPipe.Infrastructure/Interfaces/Service/ICsvRwService.cs
@@ -10,5 +10,5 @@ public interface ICsvRwService
     Task<Result<List<T>>> ReadFileAsync<T>(FileInfo path, CancellationToken ct);
     Result Write<TClass, TMap>(FileInfo path, IEnumerable<TClass> unparsedObject) where TMap : ClassMap<TClass>;
     Result Write<TClass>(IEnumerable<TClass> unparsedObject, FileInfo path);
-    Task<Result> WriteAsync<TClass>(IEnumerable<TClass> input,  FileInfo path, CancellationToken ct);
+    Task<Result> WriteAsync<TClass>(IEnumerable<TClass> input, FileInfo path, CancellationToken ct = default);
 }

315d38610d0f50d11fbcb8dfa4f519475852db37
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-24 11:54:57 -0600

    Add YellerCsvReporterAsync.cs

---
+++ LeadPipe.Infrastructure/Service/Report/YellerCsvReporterAsync.cs
@@ -0,0 +1,22 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Service;
+using LeadPipe.Infrastructure.Settings;
+
+namespace LeadPipe.Infrastructure.Service.Report;
+
+public sealed class YellerCsvReporterAsync(
+    IYellerSettings settings,
+    ICsvRwService csv
+    ) : IReport<ReportYeller>
+{
+    private readonly FileInfo _loc = new(settings.YellerCsvReporterLoc!);
+    private readonly ICsvRwService _csv = csv;
+
+    public async Task<Result> ReportData(List<ReportYeller> d)
+    {
+        Result result = await _csv.WriteAsync(d, _loc);
+        return result;
+    }
+}

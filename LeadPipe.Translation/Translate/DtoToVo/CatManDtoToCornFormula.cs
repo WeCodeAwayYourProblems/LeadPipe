30dcb77034b587a9ce2e89b759ec5af5217150c3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-24 15:39:57 -0600

    Update CatManDtoToCornFormula.cs

---
+++ LeadPipe.Translation/Translate/DtoToVo/CatManDtoToCornFormula.cs
@@ -0,0 +1,34 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+using LeadPipe.Translation.Translate.VoToEntity;
+
+namespace LeadPipe.Translation.Translate.DtoToVo;
+
+internal sealed class CatManDtoToCornFormula : IDtoToVo<CatManDto, CornFormula>
+{
+    public CornFormula Translate(CatManDto data)
+    {
+        long unix = (long)(data.unix_time is null ? 0 : data.unix_time);
+        DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(unix);
+
+        PhoneNumber number = new(data.caller_number_bare);
+
+        string payLoad = data.ToString();
+        string referring = data.location ?? "None";
+        string metaData = $"{CornMySqlEntityTranslationHelper.ReferringValue}{referring}";
+        string source = data.source ?? "Unknown";
+
+        var result = new CornFormula(
+            Id: data.id,
+            PhoneNumber: number,
+            Date: date,
+            PayLoad: payLoad,
+            MetaData: metaData,
+            Source: source
+            );
+
+        return result;
+    }
+
+}

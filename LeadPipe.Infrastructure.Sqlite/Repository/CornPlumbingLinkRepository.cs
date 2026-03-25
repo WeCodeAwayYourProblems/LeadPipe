7aa0ae7f7e5c151ae993a527d4a4a24894f9b5b0
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-25 14:09:38 -0600

    Update CornPlumbingLinkRepository.cs

--- LeadPipe.Infrastructure.Sqlite/Repository/CornPlumbingLinkRepository.cs
+++ LeadPipe.Infrastructure.Sqlite/Repository/CornPlumbingLinkRepository.cs
@@ -20,23 +20,28 @@ public sealed class CornPlumbingLinkRepository
             .Include(q => q.PlumbingEntity);
     }
 
-    protected override UpsertFields LinkDetails { get; } = new(
+    protected override UpsertFields LinkDetails { get; } =
+    new(
         TableName: TableNames.CornPlumbingLinksName,
         TempTable: $"temp_{TableNames.CornPlumbingLinksName}",
         Id1: nameof(CornPlumbingLink.CornId),
         Id2: nameof(CornPlumbingLink.PlumbingId),
         PhoneCol: nameof(CornPlumbingLink.MatchingPhone),
         DateCol: nameof(CornPlumbingLink.UnixMatchDate),
-        EntityName: nameof(CornPlumbingLink)
+        EntityName: nameof(CornPlumbingLink),
+        ColumnCount: 4
         );
 
-    protected override ParentFields Parent => new(
+    protected override ParentFields Parent { get; } =
+    new(
         Parent1Name: TableNames.CornEntitiesName,
         Parent1Id: nameof(CornEntity.Id),
         Parent2Name: TableNames.PlumbingEntitiesName,
         Parent2Id: nameof(PlumbingEntity.Id)
     );
 
+    private static int[]? _columnIndexes;
+    protected override int[] ColumnIndexes => _columnIndexes ??= [.. Enumerable.Range(0, LinkDetails.ColumnCount)];
     protected override async Task AddLinks(List<CornPlumbingLink> links, int batchSize, CancellationToken ct)
     {
         for (int i = 0; i < links.Count; i += batchSize)
@@ -49,15 +54,27 @@ public sealed class CornPlumbingLinkRepository
             {
                 var link = batch[j];
 
-                int o = j * 4;
-                rows.Add($"({{{o}}}, {{{o + 1}}}, {{{o + 2}}}, {{{o + 3}}})");
+                int o = j * LinkDetails.ColumnCount;
+                var placeholders = ColumnIndexes.Select(columnIndex => $"{{{o + columnIndex}}}");
+                rows.Add($"({string.Join(", ", placeholders)})");
+
+                // Order here must match order below
                 values.Add(link.CornId);
                 values.Add(link.PlumbingId);
                 values.Add(link.MatchingPhone);
                 values.Add(link.UnixMatchDate);
             }
 
-            string joined = $"INSERT INTO {LinkDetails.TempTable} VALUES {string.Join(",", rows)}";
+            // Order here must match order above
+            string joined = $"""
+                INSERT INTO {LinkDetails.TempTable} (
+                    {nameof(CornPlumbingLink.CornId)},
+                    {nameof(CornPlumbingLink.PlumbingId)},
+                    {nameof(CornPlumbingLink.MatchingPhone)},
+                    {nameof(CornPlumbingLink.UnixMatchDate)}
+                ) 
+                VALUES {string.Join(",", rows)}
+                """;
             await _context.Database.ExecuteSqlRawAsync(joined, values, ct);
         }
     }

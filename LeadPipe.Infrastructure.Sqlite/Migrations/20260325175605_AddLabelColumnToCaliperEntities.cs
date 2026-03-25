993eed0245b516cd2465e40928b7b6f42b55f4e2
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-25 14:06:04 -0600

    Update Migrations

---
+++ LeadPipe.Infrastructure.Sqlite/Migrations/20260325175605_AddLabelColumnToCaliperEntities.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeadPipe.Infrastructure.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLabelColumnToCaliperEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Label",
+                table: "CaliperEntities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Label",
+                table: "CaliperEntities");
+        }
+    }
+}

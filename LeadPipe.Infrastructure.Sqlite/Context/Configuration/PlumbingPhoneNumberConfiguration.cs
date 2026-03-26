157b6567fe9f90be7bb23179cd3bc4e442772ab1
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-26 13:02:08 -0600

    Add PlumbingPhoneNumberConfiguration.cs

---
+++ LeadPipe.Infrastructure.Sqlite/Context/Configuration/PlumbingPhoneNumberConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeadPipe.Infrastructure.Sqlite.Context.Configuration;
+
+internal sealed class PlumbingPhoneNumberConfiguration : IEntityTypeConfiguration<PlumbingPhoneNumber>
+{
+    public void Configure(EntityTypeBuilder<PlumbingPhoneNumber> builder)
+    {
+        var phone = builder.ToTable(TableNames.PlumbingPhoneNumbersName);
+        phone.HasKey(p => p.Id);
+        phone.Property(p => p.Id)
+            .ValueGeneratedOnAdd();
+
+        var pNumber = phone.Property(p => p.PhoneNumber);
+        pNumber.HasConversion(PlumbingConversionsHelper.PhoneNumberAndLongConversion);
+        pNumber.Metadata.SetValueComparer(PlumbingConversionsHelper.PhoneNumberComparer);
+        pNumber.IsRequired();
+
+        // Foreign key
+        phone.Property(p => p.PlumbingId)
+            .IsRequired();
+
+        phone.HasIndex(p => new { p.PlumbingId, p.PhoneNumber })
+            .IsUnique();
+
+        phone.HasOne(p => p.Plumbing)
+            .WithMany(p => p.PhoneNumbers)
+            .HasForeignKey(p => p.PlumbingId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

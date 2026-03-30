43883f245a786265852f3827e1ee5c7b67caf7ea
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-30 10:10:09 -0600

    Update Plumbing.cs

--- LeadPipe.Domain/ValueObjects/Plumbing.cs
+++ LeadPipe.Domain/ValueObjects/Plumbing.cs
@@ -1,3 +1,3 @@
 ﻿namespace LeadPipe.Domain.ValueObjects;
 
-public record Plumbing(long Id, PhoneNumber PhoneNumber, DateTimeOffset Date, string? Contents, string? Branch, string MetaData, Source Source);
+public record Plumbing(long Id, PhoneNumber PhoneNumber, DateTimeOffset Date, string? Contents, string? Branch, string MetaData, Source Source, PhoneNumber[]? Numbers);

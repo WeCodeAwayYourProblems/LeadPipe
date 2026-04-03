b3e8a6b396ec403a5f86ea216871e6f542999bb3
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-03 14:57:15 -0600

    Add PlumbingMetaDataCanonicalPersistenceFormat.cs

---
+++ LeadPipe.Translation/Primitives/PlumbingMetaDataCanonicalPersistenceFormat.cs
@@ -0,0 +1,9 @@
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Translation.Primitives;
+
+internal class PlumbingMetaDataCanonicalPersistenceFormat : IPlumbingMetaDataCanonicalPersistenceFormat<PlumbingEntity, string>
+{
+    public string Translate(PlumbingEntity t) => t.MetaData is null ? string.Empty : t.MetaData;
+}

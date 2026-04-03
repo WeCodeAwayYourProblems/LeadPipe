3428876a6d9aa93d790f45a38959e0d84bfc17b7
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-03 14:51:17 -0600

    Update IDtoToVo.cs

--- LeadPipe.Infrastructure/Interfaces/Translate/IDtoToVo.cs
+++ LeadPipe.Infrastructure/Interfaces/Translate/IDtoToVo.cs
@@ -1,6 +1,3 @@
 ﻿namespace LeadPipe.Infrastructure.Interfaces.Translate;
 
-public interface IDtoToVo<TDto, TVo>
-{
-    TVo Translate(TDto data);
-}
+public interface IDtoToVo<TDto, TVo>: ITranslate<TDto, TVo> { }

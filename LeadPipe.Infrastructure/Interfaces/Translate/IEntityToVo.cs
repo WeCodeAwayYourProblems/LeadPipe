b748f07dfa09152205f30f1495d92d79ab7cd610
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-03 14:51:47 -0600

    Update IEntityToVo.cs

--- LeadPipe.Infrastructure/Interfaces/Translate/IEntityToVo.cs
+++ LeadPipe.Infrastructure/Interfaces/Translate/IEntityToVo.cs
@@ -1,9 +1,6 @@
 ﻿namespace LeadPipe.Infrastructure.Interfaces.Translate;
 
-public interface IEntityToVo<TEntity, TVo>
-{
-    TVo Translate(TEntity entity);
-}
+public interface IEntityToVo<TEntity, TVo> : ITranslate<TEntity, TVo> { }
 public interface IEntityToVo<TEntity1, TEntity2, TVo>
 {
     TVo Translate(TEntity1 e1, TEntity2 e2);

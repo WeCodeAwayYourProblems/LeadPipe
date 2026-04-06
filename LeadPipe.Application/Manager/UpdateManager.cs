2850752dabfa743a7c9817a231150c783e59cf21
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-06 09:35:47 -0600

    Update UpdateManager.cs

--- LeadPipe.Application/Manager/UpdateManager.cs
+++ LeadPipe.Application/Manager/UpdateManager.cs
@@ -6,8 +6,8 @@ namespace LeadPipe.Application.Manager;
 
 public interface IUpdateManager
 {
-    Task<Result> Manage(Source source, bool refresh);
-    Task<Result> Manage(bool refresh);
+    Task<Result> Manage(Source source, bool refresh, bool forceRun);
+    Task<Result> Manage(bool refresh, bool forceRun);
 }
 internal sealed class UpdateManager(
     ISourceDataUpdateManager sourceData,
@@ -18,18 +18,18 @@ internal sealed class UpdateManager(
     readonly ISourceDataUpdateManager _source = sourceData;
     readonly ICoreDataUpdateManager _core = coreData;
     readonly IAssociationManager _associate = associate;
-    public async Task<Result> Manage(Source source, bool refresh)
+    public async Task<Result> Manage(Source source, bool refresh, bool forceRun)
     {
-        var core = await _core.Manage(refresh);
+        var core = await _core.Manage(refresh, forceRun);
         var sourceData = await _source.Manage(refresh, source);
         var associate = await _associate.Manage();
         var combined = Result.Combine(core, sourceData, associate);
         return combined;
     }
 
-    public async Task<Result> Manage(bool refresh)
+    public async Task<Result> Manage(bool refresh, bool forceRun)
     {
-        var core = await _core.Manage(refresh);
+        var core = await _core.Manage(refresh, forceRun);
         var sourceData = await _source.Manage(refresh);
         var associate = await _associate.Manage();
         var combined = Result.Combine(core, sourceData, associate);

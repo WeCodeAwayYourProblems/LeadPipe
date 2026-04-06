1c600c5c425ef3bd4fd92054f38ed6451d310df2
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-06 15:10:36 -0600

    Add HashSetExtensions.cs

---
+++ LeadPipe.Infrastructure/Service/HashSetExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace LeadPipe.Infrastructure.Service;
+
+public static class HashSetExtensions
+{
+    public static HashSet<TResult> ToHashSetFast<TSource, TResult>(
+        this ICollection<TSource> source,
+        Func<TSource, TResult> selector)
+    {
+        HashSet<TResult> result = new(source.Count);
+        foreach (TSource? item in source)
+            result.Add(selector(item));
+
+        return result;
+    }
+}

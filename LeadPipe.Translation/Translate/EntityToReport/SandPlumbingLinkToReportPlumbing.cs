24c9aaa24160598188529dd6ce673f35d2612cfb
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-07 09:01:14 -0600

    Update SandPlumbingLinkToReportPlumbing.cs

--- LeadPipe.Translation/Translate/EntityToReport/SandPlumbingLinkToReportPlumbing.cs
+++ LeadPipe.Translation/Translate/EntityToReport/SandPlumbingLinkToReportPlumbing.cs
@@ -1,20 +1,25 @@
 ﻿using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Translate;
 using System.Text.RegularExpressions;
 
 namespace LeadPipe.Translation.Translate.EntityToReport;
 
-internal partial class SandPlumbingLinkToReportPlumbing : IEntityToReport<SandPlumbingLink, ReportPlumbing>
+internal partial class SandPlumbingLinkToReportPlumbing(IClock clock) : IEntityToReport<SandPlumbingLink, ReportPlumbing>
 {
+    private readonly IClock _clock = clock;
+
+    private readonly static DateTimeOffset _twentyTwelve = new(2012, 1, 1, 0, 0, 0, TimeSpan.Zero);
     private const string dateFormat = "yyyy-MM-dd HH:mm:ss";
-    public ReportPlumbing Translate(SandPlumbingLink link)
+    private const string _unk = "Unknown";
+    public ReportPlumbing Translate(SandPlumbingLink t)
     {
-        if (link.PlumbingEntity is not PlumbingEntity plumb)
+        if (t.PlumbingEntity is not PlumbingEntity plumb)
             throw new Exception("Plumbing entities cannot be null");
 
-        if (link.SandEntity is not SandEntity sub)
-            sub = new()
+        if (t.SandEntity is not SandEntity sand)
+            sand = new()
             {
                 CustardId = 0,
                 Id = 0,
@@ -28,54 +33,64 @@ internal partial class SandPlumbingLinkToReportPlumbing : IEntityToReport<SandPl
         long phoneNumber = plumb.PhoneNumber.Number;
 
         DateTimeOffset date = DateTimeOffset.FromUnixTimeSeconds(plumb.UnixDate);
-        string formattedDate = date.ToString(dateFormat);
 
-        string message = plumb.Contents is string c 
-            ? NewLineRegex().Replace(c.Replace(',', ';')," | ") 
+        string contents = plumb.Contents is string c
+            ? NewLineRegex().Replace(
+                c.Replace(',', ';'), 
+                " | ")
             : string.Empty;
         string source = plumb.Source.ToString();
         string metadata = plumb.MetaData;
 
-        long customerId = sub.CustardId;
-        long subId = sub.Id;
-        bool subActive = sub.Active;
-        bool completed = sub.Complete;
+        long custardId = sand.CustardId;
+        long sandId = sand.Id;
+        bool sandActive = sand.Active;
+        bool completed = sand.Complete;
 
         // Dates
-        DateTimeOffset custDate = DateTimeOffset.FromUnixTimeSeconds(sub.UnixDate);
-        string formattedCustDate = custDate.ToString(dateFormat);
+        DateTimeOffset? custDate = 
+            _twentyTwelve.ToUnixTimeSeconds() >= sand.UnixDate || _clock.UtcNow.ToUnixTimeSeconds() <= sand.UnixDate
+                ? null
+                : DateTimeOffset.FromUnixTimeSeconds(sand.UnixDate);
 
-        DateTimeOffset custCxlDate = DateTimeOffset.FromUnixTimeSeconds(sub.UnixCancelDate);
+        DateTimeOffset? custCxlDate = 
+            _twentyTwelve.ToUnixTimeSeconds() >= sand.UnixCancelDate || _clock.UtcNow.ToUnixTimeSeconds() <= sand.UnixCancelDate
+                ? null
+                : DateTimeOffset.FromUnixTimeSeconds(sand.UnixCancelDate);
 
-        DateTimeOffset subDate = DateTimeOffset.FromUnixTimeSeconds(sub.UnixDate);
-        string formattedSubDate = subDate.ToString(dateFormat);
+        DateTimeOffset? subDate = custDate;
 
-        DateTimeOffset subCxlDate = DateTimeOffset.FromUnixTimeSeconds(sub.UnixCancelDate);
+        DateTimeOffset? subCxlDate = custCxlDate;
 
         bool msgBeforeCust = date < custDate && date < subDate;
         bool isSale = msgBeforeCust && completed;
 
+        string s1 = sand.Seller == 0 ? _unk : sand.Seller.ToString();
+        string s2 = sand.Seller2 == 0 ? string.Empty : $" | {sand.Seller2}";
+        string s3 = sand.Seller3 == 0 ? string.Empty : $" | {sand.Seller3}";
+        string sellers = $"{s1}{s2}{s3}";
+
         ReportPlumbing result = new()
         {
-            MsgBeforeCust = msgBeforeCust,
-            IsSale = isSale,
             PhoneNumber = phoneNumber,
             Date = date,
-            FormattedDate = formattedDate,
-            Message = message,
+            Contents = contents,
             Source = source,
-            MetaData = metadata,
-            CustomerId = customerId,
-            SubId = subId,
-            SubActive = subActive,
+            MsgBC = msgBeforeCust,
+            IsS = isSale,
+            CustardId = custardId,
+            SandActive = sandActive,
+            CustardDate = custDate,
+            CustardCxlDate = custCxlDate,
+            SandId = sandId,
             Completed = completed,
-            CustDate = custDate,
-            FormattedCustDate = formattedCustDate,
-            CustCxlDate = custCxlDate,
-            SubDate = subDate,
-            FormattedSubDate = formattedSubDate,
-            SubCxlDate = subCxlDate,
+            Value = sand.Value,
+            SandDate = subDate,
+            SandCxlDate = subCxlDate,
+            Sellers = sellers,
+            MetaData = metadata,
         };
+
         return result;
     }
 

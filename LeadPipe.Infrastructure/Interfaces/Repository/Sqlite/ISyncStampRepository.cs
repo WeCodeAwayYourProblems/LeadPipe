85fc3ddcfe1105bc9d2df980eb5c90c3a235e8c1
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-09 10:20:39 -0600

    Update ISyncStampRepository.cs Added comments

--- LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/ISyncStampRepository.cs
+++ LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/ISyncStampRepository.cs
@@ -4,6 +4,9 @@ using LeadPipe.Infrastructure.Entity.Sqlite;
 
 namespace LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
 
+/// <summary>
+/// Saves the timestamp and success state of the last run for a particular <see cref="SyncKey"/>
+/// </summary>
 public interface ISyncStampRepository
 {
     Task<Result<SyncStampEntity>> GetByKeyAsync(Source? source, SyncKey key);

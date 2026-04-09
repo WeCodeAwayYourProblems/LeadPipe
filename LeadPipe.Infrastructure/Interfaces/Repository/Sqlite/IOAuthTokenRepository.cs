719f4c6d5682d2852ebca1cb6ce723c2cbf32e1d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-09 16:49:57 -0600

    Add IOAuthTokenRepository.cs

---
+++ LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/IOAuthTokenRepository.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+
+namespace LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+
+public interface IOAuthTokenRepository
+{
+    Task<Result<OAuthTokenEntity>> GetByProviderAsync(string providerName, CancellationToken ct = default);
+    Task<Result<OAuthTokenEntity>> UpsertAsync(OAuthTokenEntity token, CancellationToken ct = default);
+}

32315f3ed8905715359010f94c32192f2a732f0a
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-09 15:34:34 -0600

    Update TokenDtoToOAuthTokenEntity.cs

---
+++ LeadPipe.Translation/Translate/Translate/TokenDtoToOAuthTokenEntity.cs
@@ -0,0 +1,27 @@
+﻿using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Translation.Translate.Translate;
+
+internal class TokenDtoToOAuthTokenEntity(IClock clock) : ITranslate<TokenDto, OAuthTokenEntity>
+{
+    private readonly IClock _clock = clock;
+    public OAuthTokenEntity Translate(TokenDto t)
+    {
+        var now = _clock.UtcNow;
+        var expiresAt = DateTimeOffset.FromUnixTimeSeconds(now.ToUnixTimeSeconds() + t.ExpiresIn);
+        var result = new OAuthTokenEntity
+        {
+            AccessToken = t.AccessToken,
+            TokenType = t.TokenType,
+            RefreshToken = t.RefreshToken,
+            ExpiresAtUtc = expiresAt,
+            UnixExpiresAtUtc = expiresAt.ToUnixTimeSeconds(),
+            UpdatedAtUtc = now,
+            UnixUpdatedAtUtc = now.ToUnixTimeSeconds()
+        };
+        return result;
+    }
+}

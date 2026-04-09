bbdb438b9ed0818f0d0f50d73ebdf58c65399d14
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-09 16:50:23 -0600

    Add OAuthTokenConfiguration.cs

---
+++ LeadPipe.Infrastructure.Sqlite/Context/Configuration/OAuthTokenConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeadPipe.Infrastructure.Sqlite.Context.Configuration;
+
+internal sealed class OAuthTokenConfiguration : IEntityTypeConfiguration<OAuthTokenEntity>
+{
+    public void Configure(EntityTypeBuilder<OAuthTokenEntity> builder)
+    {
+        builder.ToTable(TableNames.OAuthTokensName);
+        builder.HasKey(x => x.Provider);
+        builder.Property(x => x.Provider)
+            .IsRequired();
+        builder.Property(x => x.AccessToken)
+            .IsRequired();
+        builder.Property(x => x.TokenType)
+            .IsRequired();
+        builder.Property(x => x.ExpiresAtUtc)
+            .IsRequired();
+        builder.Property(x => x.UnixExpiresAtUtc)
+            .IsRequired();
+        builder.Property(x => x.UpdatedAtUtc)
+            .IsRequired();
+        builder.Property(x => x.UnixUpdatedAtUtc)
+            .IsConcurrencyToken() // Redundant but safe
+            .IsRequired();
+    }
+}

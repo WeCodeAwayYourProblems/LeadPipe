6bfcc467dfed7050eae3f0822282cbc3683c119a
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-13 12:25:33 -0600

    Update IOAuthTokenProvider.cs

--- LeadPipe.Infrastructure/Interfaces/Api/IOAuthTokenProvider.cs
+++ LeadPipe.Infrastructure/Interfaces/Api/IOAuthTokenProvider.cs
@@ -1,10 +1,11 @@
 ﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Api;
 
 namespace LeadPipe.Infrastructure.Interfaces.Api;
 
 internal interface IOAuthTokenProvider
 {
+    Task<Result<AccessToken>> ForceRefreshAsync(CancellationToken ct);
     Task<Result<string>> GetValidAccessTokenAsync(CancellationToken ct);
-    Task<Result<string>> ForceRefreshAsync(CancellationToken ct);
 }
 

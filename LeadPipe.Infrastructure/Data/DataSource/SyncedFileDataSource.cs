01dece1a92f4c34e0946d19fe28acaea5de83449
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-30 09:37:43 -0600

    Update SyncedFileDataSource.cs

--- LeadPipe.Infrastructure/Data/DataSource/SyncedFileDataSource.cs
+++ LeadPipe.Infrastructure/Data/DataSource/SyncedFileDataSource.cs
@@ -93,7 +93,7 @@ public abstract class SyncedFileDataSource<TDto>(
         if (latest.IsFailure)
             return DefaultLatestDate;
 
-        DateTimeOffset result = DateTimeOffset.FromUnixTimeSeconds(latest.Value.UnixLastSyncUtc); // Use UnixLastSyncUtc to prevent date drift
+        DateTimeOffset result = DateTimeOffsetExt.FromUnixTime(latest.Value.UnixLastSyncUtc); // Use UnixLastSyncUtc to prevent date drift
 
         return result;
     }

1aed125a42113f77025534d6abcd442c2f3250e8
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-05-18 13:53:38 -0600

    Add LabReport.sql

---
+++ Batch/LabReport.sql
@@ -0,0 +1,16 @@
+/*Lab Report*/
+select 
+    p.phonenumber as `Phone Number`, p.date as `Date of Message`, p.contents as `Message Contents`, p.source as `Message Source`,
+    1 as `IM Lead`, 1 as `Sales Lead`, c.id as `Customer ID`, s.active as `Subscription is Active`, c.date as `Customer record start date`, c.unixcanceldate as `Customer cancel date Unix`,
+    s.id as `Subscription Id`, s.complete as `Completed Initial`, s.value as `Contract Value`, s.date as `Subscription Start Date`, s.unixcanceldate as `Subscription cancel date Unix`,
+    s.type as `Service Type`, 
+    CASE 
+        WHEN instr(p.metadata, 'Emails:') > 0 
+        THEN substr(p.metadata, instr(p.metadata, 'Emails:') + 7)
+        ELSE NULL
+    END AS Email
+from plumbingentities p
+left join custardentities c on p.phonenumber in (c.phonenumber, c.phonenumber2)
+left join sandentities s on s.custardid = c.id
+where p.phonenumber > 0 and p.source = 'Lab'
+order by p.id asc;

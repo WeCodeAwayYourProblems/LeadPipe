a9308372947e1def410231571fda31cf1fc02f09
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-05-18 15:44:32 -0600

    Update LabReport.bat

--- Batch/LabReport.bat
+++ Batch/LabReport.bat
@@ -1,6 +1,32 @@
 @echo off
 
+title Lab Query
+
 set BASE=%USERPROFILE%\Repos\LeadPipe\LeadPipe.Infrastructure\.info
 set BASE2=%USERPROFILE%\Repos\Automate\Automate.Infrastructure\.info\Reports
 
-sqlite3 -header -csv "%BASE%\leadpipe.test.db" < "%BASE%\LabReport.sql" > "%BASE2%\LabReport_Test.csv"
+set database="%BASE%\leadpipe.test.db"
+set output="%BASE2%\LabReport_Test.csv"
+
+sqlite3 -header -csv %database% < "%BASE%\LabReport.sql" > %output%
+
+set error=%errorlevel%
+echo.
+echo Lab Query success: %error%
+:: error messages are placed in the output file
+if not "%error%"="0" (
+    type %output%
+    goto :pauseExecution
+)
+echo.
+
+:: Ending
+echo All Executions successful!
+goto :end
+
+:pauseExecution
+echo Query failed
+pause 
+
+echo.
+:end

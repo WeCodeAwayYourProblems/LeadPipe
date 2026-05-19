af89c683202f645ba6b66338b659781b0e675f74
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-05-19 17:43:00 -0600

    Update AllReport.sql

--- LeadPipe.Infrastructure/.queries/AllReport.sql
+++ LeadPipe.Infrastructure/.queries/AllReport.sql
@@ -1,35 +1,34 @@
 /*All Report*/
 select 
-    p.phonenumber as `Phone Number`, 
-    p.date as `Date of Message`, 
-    p.contents as `Message Contents`, 
-    p.source as `Message Source`,
-    if(p.unixdate < s.unixdate and p.unixdate < c.unixdate, 1, 0) as `IM Lead`, 
-    1 as `Potential Sales Lead`, 
-    c.id as `Customer ID`, 
-    s.active as `Subscription is Active`, 
-    c.date as `Customer record start date`, 
-    c.unixcanceldate as `Customer unix cxl date`,
-    s.id as `Subscription Id`, 
-    s.complete as `Completed Initial`, 
-    s.value as `Contract Value`, 
-    s.date as `Subscription Start Date`, 
-    s.unixcanceldate as `Subscription unix cxl date`,
-    s.type as `Service Type`, 
+    p.phonenumber AS `Phone Number`, 
+    p.date AS `Date of Message`, 
+    p.contents AS `Message Contents`, 
+    p.source AS `Message Source`,
+    if(p.unixdate < s.unixdate AND p.unixdate < c.unixdate, 1, 0) AS `IM Lead`, 
+    1 AS `Potential Sales Lead`, 
+    c.id AS `Customer ID`, 
+    s.active AS `Subscription is Active`, 
+    c.date AS `Customer record start date`, 
+    c.unixcanceldate AS `Customer unix cxl date`,
+    s.id AS `Subscription Id`, 
+    s.complete AS `Completed Initial`, 
+    s.value AS `Contract Value`, 
+    s.date AS `Subscription Start Date`, 
+    s.unixcanceldate AS `Subscription unix cxl date`,
+    s.type AS `Service Type`, 
+    IF(p.unixdate < s.unixdate AND p.unixdate < c.unixdate AND s.active = 1, 1, 0) AS `Sale`,
     CASE 
         WHEN instr(p.metadata, 'Emails:') > 0 
-        THEN substr(p.metadata, instr(p.metadata, 'Emails:') + 7)
+            THEN substr(p.metadata, instr(p.metadata, 'Emails:') + 7)
+        WHEN instr(p.metadata, 'ID: ') > 0 
+            THEN substr(p.metadata, instr(p.metadata, 'ID: ') + 4)
         ELSE NULL
-    END AS `Email`,
+    END AS `Unique Item`,
+
 /*For debugging*/
-    p.id as `PlumbingId`, 
-    CASE
-        WHEN instr(p.metadata, 'ID: ') > 0
-        THEN substr(p.metadata, instr(p.metadata, 'ID: ') + 4)
-        ELSE NULL
-    END AS 'YellerId'
-from plumbingentities p
-left join custardentities c on p.phonenumber in (c.phonenumber, c.phonenumber2)
-left join sandentities s on s.custardid = c.id
-where p.phonenumber > 0
-order by p.id asc;
+    p.id AS `PlumbingId`
+FROM plumbingentities AS p
+LEFT JOIN custardentities AS c ON p.phonenumber IN (c.phonenumber, c.phonenumber2)
+LEFT JOIN sandentities AS s ON s.custardid = c.id AND s.active = 1
+WHERE p.phonenumber > 0
+ORDER BY p.id ASC;

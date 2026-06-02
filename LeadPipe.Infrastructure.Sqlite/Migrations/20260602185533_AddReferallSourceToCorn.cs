e69ef904a3a8b646092bf5f641a525e8cc6f450c
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-06-02 14:27:18 -0600

    Update Migrations

---
+++ LeadPipe.Infrastructure.Sqlite/Migrations/20260602185533_AddReferallSourceToCorn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeadPipe.Infrastructure.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReferallSourceToCorn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReferralSource",
+                table: "CornEntities",
+                type: "TEXT",
+                maxLength: 45,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferralSource",
+                table: "CornEntities");
+        }
+    }
+}
